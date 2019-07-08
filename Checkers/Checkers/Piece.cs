259c822a707da58836ebfa54c681761df448cff4
Patrick O' Riordan <keshmedia@users.noreply.github.com>
2019-07-08 11:34:26 +0100

    Add files via upload
    Checkers

---
+++ Checkers/Checkers/Piece.cs
@@ -0,0 +1,25 @@
+﻿namespace CheckersBoard
+{
+    class Piece
+    {
+ public int Row { get; set; }
+ public int Column { get; set; }
+
+ public Piece(int row, int col)
+ {
+            this.Row = row;
+            this.Column = col;
+ }
+ public override bool Equals(System.Object obj)
+ {
+ if (obj == null)
+ {
+     return false;
+ }
+ Piece piece = obj as Piece;
+ if ((System.Object)piece == null)
+ {
+     return false;
+ }
+ return Row == piece.Row && Column == piece.Column;
+        }  }}
